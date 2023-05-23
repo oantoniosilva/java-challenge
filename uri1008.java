671b7a101ea0ecafd95e1f942f5b36a7c20085b2
Antonio Silva <antoniopsilva083@gmail.com>
2023-05-22 23:11:24 -0300

    Update uri1008.java

--- uri1008.java
+++ uri1008.java
@@ -1,3 +1,7 @@
+/* Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+decimais. */
+
 import java.util.Locale;
 import java.util.Scanner;
 
